cf883b6468bfee2e76e97257c6552725be3ff317
Krittamook Aksornchindarat <krittamook@hotmail.com>
2021-07-01 14:15:23 +0700

    CI/CD testing 3

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listerner
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
-  console.log('making changes...');
+  console.log('making changes2...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KET must be deifined');
   }
